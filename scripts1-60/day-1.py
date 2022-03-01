cf5985403ace96bdb93dc11f255e9f7678a6a87e
ammett <32433064+jesuispy@users.noreply.github.com>
2022-03-01 13:05:11 -0500

    Day-1 string manipulation

--- scripts1-60/day-1.py
+++ scripts1-60/day-1.py
@@ -1,1 +1,22 @@
+id = "Cloud"
+print(id.upper())
+print(id.lower())
 
+greeting = "Hello " + id +   " How are you!"
+print(greeting)
+
+#length of string converted to string
+print(str(len(greeting)) + " Is the lenght of the greeting")
+
+#if function 
+if id in greeting:
+    print("The id is contained in the greeting!")
+else: 
+    print("The id is hidden")
+
+print(f"The first character of the id is {id[0]}")
+
+scream = id * 5
+greeting = "I'm gonna repeat it " + scream +   " How are you!"
+print(f"My name is {scream}")
+print(greeting)
